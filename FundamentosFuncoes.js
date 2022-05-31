666d854e197d7be9369e720cad5420b7a80e0ad9
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2022-05-31 13:51:41 -0300

    Segundo Commit

--- FundamentosFuncoes.js
+++ FundamentosFuncoes.js
@@ -4,6 +4,7 @@ function imprimirSoma(a,b){
     console.log(a+b)
 }
 imprimirSoma(2,3);
-imprimirSoma(2);//não funciona pois não tem o segundo parametro
-imprimirSoma(2,3,4,5,6,7,8,9);//não funciona pois não tem o segundo parametro
+imprimirSoma(2);//entendera o segundo termo como indefinido
+imprimirSoma(2,3,4,5,6,7,8,9);//vai somar os dois primeiros apenas
 imprimirSoma();//não funciona pois não tem o primeiro parametro
+//
