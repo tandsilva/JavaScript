6ba868af2863ff4ab56837ed4e173c23f3430d05
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2022-06-01 13:45:09 -0300

    Commit 10

--- ForComVar.js
+++ ForComVar.js
@@ -9,3 +9,14 @@ for(let i;i<10;i++){
     console.log(i);
 }
 console.log(i);//erro pois i nao foi inicializado fora, apenas  dentro do escopo
+
+
+const func = []
+for(var i = 0;i<10;i++){
+    func.push(function(){
+        console.log(i)
+    })
+}
+console.log(func[2])//vai imprimir 10 pois o i esta dentro do escopo da funcao
+func[2]()//vai imprimir 10 pois o i esta dentro do escopo da funcao
+console.log(func[8])//vai imprimir 10 pois o i esta dentro do escopo da funcao
