7c98c40e20f97861102e20cb3ceb8a75cc129d64
Thiago Aldrin Marques da Silva <65571896+tandsilva@users.noreply.github.com>
2022-06-01 13:23:04 -0300

    Commit 8

---
+++ ForComVar.js
@@ -0,0 +1,11 @@
+for(var i;i<10;i++){
+    console.log(i);
+}
+ 
+
+//for com let 
+
+for(let i;i<10;i++){
+    console.log(i);
+}
+console.log(i);//erro pois i nao foi inicializado fora, apenas  dentro do escopo
